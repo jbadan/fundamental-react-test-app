ccf8a2bf1811e53690f4c0fc05109bb41b4adf36
jbadan <jenna.badanowski@gmail.com>
2020-06-05 08:46:41 -0700

    add fonts

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
+import FormGroup from 'fundamental-react/Forms/FormGroup';
+import FormInput from 'fundamental-react/Forms/FormInput';
+import FormItem from 'fundamental-react/Forms/FormLabel';
+import FormLabel from 'fundamental-react/Forms/FormLabel';
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header">Create React App with Fundamental React</header>
+      <div className="App-container">
+        <FormGroup>
+        <FormItem>
+            <FormLabel htmlFor='input-1'>
+                Default Input
+            </FormLabel>
+            <FormInput
+                id='input-1'
+                placeholder='Field placeholder text' />
+        </FormItem>
+        </FormGroup>
+        <FormGroup>
+          <FormItem>
+              <FormLabel htmlFor='input-1'>
+                  Default Input
+              </FormLabel>
+              <FormInput
+                  id='input-1'
+                  placeholder='Field placeholder text' />
+          </FormItem>
+        </FormGroup>
+    
+    </div>
     </div>
   );
 }
